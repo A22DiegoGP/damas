423d4ca65d2dd286435a79beca51838f4c9b98ea
daw1 - García Piñeiro <A22DiegoGP@sanclemente.local>
2022-12-09 10:11:08 +0100

    Inicio proyecto

---
+++ src/main/java/com/damas/DamasApp.java
@@ -0,0 +1,13 @@
+package com.damas;
+
+/**
+ * Hello world!
+ *
+ */
+public class DamasApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
